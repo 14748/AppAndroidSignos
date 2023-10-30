775106b4bcd75db712890c16bedfcf6ecdee83f1
14748 <sproplayerxd@gmail.com>
2023-10-30 11:01:25 +0100

    feat(MainActivity): add base code for commiting and retrieving db changes

--- app/src/main/java/com/example/appsignos/activities/MainActivity.java
+++ app/src/main/java/com/example/appsignos/activities/MainActivity.java
@@ -6,14 +6,32 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.os.Bundle;
 
 import com.example.appsignos.R;
+import com.example.appsignos.models.Categoria;
+import com.example.appsignos.models.Palabra;
+
+import io.realm.Realm;
+import io.realm.RealmResults;
 
 public class MainActivity extends AppCompatActivity {
 
     RecyclerView recyclerView;
+    Realm realm;
+    RealmResults<Categoria> resultsCategoria;
+    RealmResults<Palabra> resultsPalabra;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        realm = Realm.getDefaultInstance();
+
+        realm.beginTransaction();
+
+
+        realm.commitTransaction();
+
+        resultsCategoria = realm.where(Categoria.class).findAll();
+        resultsPalabra = realm.where(Palabra.class).findAll();
     }
 }
